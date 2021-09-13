04fe72b79c8dc6d430852d5c552e5846c4e39f86
Anastasiya <anatasiya.ashevtsova888@gmail.com>
2021-09-13 22:02:08 +0300

    edit switchToFrame

--- src/main/java/pages/QuickViewProductModalPage.java
+++ src/main/java/pages/QuickViewProductModalPage.java
@@ -1,7 +1,11 @@
 package pages;
 
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.SelenideElement;
 import io.qameta.allure.Step;
 import lombok.extern.log4j.Log4j2;
+
+import java.time.Duration;
 
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.switchTo;
@@ -19,10 +23,12 @@ public class QuickViewProductModalPage extends BasePage {
     @Step("Adding product to cart")
     public ProductAddedModalPage addProductToCart() {
         log.info("Switch to frame");
+        Selenide.Wait().withTimeout(Duration.ofSeconds(10));
         switchTo().frame(0);
         log.info("Click 'Add to cart' button");
         $(ADD_TO_CART_BUTTON_CSS).click();
         log.info("Switch to window");
+        Selenide.Wait().withTimeout(Duration.ofSeconds(10));
         switchTo().window(0);
         return new ProductAddedModalPage();
     }
